a280a60aa23e85bfc7c5b26a7fe00b1d2756c10c
Ali Bozlak <bozlak20aliihsan@gmail.com>
2026-06-15 21:37:35 +0300

    First commit

---
+++ src/main/java/dev/bozlak/on_muhasebe_spring_boot/OnMuhasebeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package dev.bozlak.on_muhasebe_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnMuhasebeSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnMuhasebeSpringBootApplication.class, args);
+	}
+
+}
