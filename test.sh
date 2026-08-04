9d9a980663285b92f8d55363d667bcf438669fc8
Ali Bozlak <bozlak20aliihsan@gmail.com>
2026-08-04 23:01:09 +0300

    Edited first test.sh file.

--- test.sh
+++ test.sh
@@ -1,10 +1,17 @@
 #!/bin/bash 
-echo "" && echo "--- Current Working Directory ---" && \
-pwd && echo "" && echo "" && \
-echo "--- 1. We are throwing Post Login Request ---" && \
-response_body=$(curl -X POST http://localhost:8080/api/login -H "Content-Type: application/json" -d "{\"username\" : \"bozlak\", \"password\" : \"bozlak\"}") && \
-echo "Login Request Successful. Response Body (JSON) : " && \
-echo "$response_body" && \
+echo "--- Current Working Directory ---" && \
+pwd && \
 SUCCESS_COLOR='\033[0;32m' && \
+ERROR_COLOR='\033[1;31m' && \
+INFO_COLOR='\033[0;34m' && \
 NO_COLOR='\033[0m' && \
-echo -e "${SUCCESS_COLOR}--- (1) Login Successful. We have a token ---${NO_COLOR}" && echo "" && echo "" 
+INFO_MESSAGE="[${INFO_COLOR}INFO${NO_COLOR}]" && \
+ERROR_MESSAGE="[${ERROR_COLOR}INFO${NO_COLOR}]" && \
+if [ $(pwd) = "/mnt/c/angularAndJava/on_muhasebe/on-muhasebe-spring-boot" ]; then
+    echo -e "$INFO_MESSAGE ${SUCCESS_COLOR}Correct Directory${NO_COLOR}"
+    else
+      echo -e "$ERROR_MESSAGE ${ERROR_COLOR}Wrong Directory${NO_COLOR}"
+fi && echo "" && \
+echo "--- Does Docker has a 'pre_accounting_db' container ---" && \
+# ToDo
+echo -e "$INFO_MESSAGE ${SUCCESS_COLOR} TEST PIPELINE SUCCESS ${NO_COLOR}" && echo ""
